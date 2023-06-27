d36cfb465fd92234f941188912c25a23987fa342
Nkondog A. Venceslas <nkanven@gmail.com>
2023-06-27 19:09:32 +0100

    Upload file and render file

--- rest/views.py
+++ rest/views.py
@@ -1,20 +1,34 @@
-from rest_framework import generics, views
+from rest_framework import generics, views, status
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
-from django.http import HttpResponse
-from wsgiref.util import FileWrapper
+from api import settings
+import os
 
 
 class FileUploadView(views.APIView):
     parser_classes = [FileUploadParser]
 
-    def put(self, request):
-        file_obj = request.FILES['file']
-        file_ext = file_obj.content_type.split("/")[1]
-        destination = open(file_obj.name+"."+file_ext, 'wb+')
+    def put(self, request, filename):
+        try:
+            file_obj = request.FILES['file']
+            file_ext = file_obj.content_type.split("/")[1]
+            specific_folder = settings.MEDIA_ROOT + os.sep + file_ext
 
-        for chunk in file_obj.chunks():
-            destination.write(chunk)
-        destination.close()
+            if not os.path.exists(settings.MEDIA_ROOT):
+                os.mkdir(settings.MEDIA_ROOT)
+            if not os.path.exists(specific_folder):
+                os.mkdir(specific_folder)
 
-        return Response(status=204)
+            f_path = specific_folder+os.sep+file_obj.name+"."+file_ext
+            f_url = settings.BASE_URL + settings.MEDIA_URL + file_ext + \
+                "/" + file_obj.name+"."+file_ext
+
+            destination = open(f_path, 'wb+')
+
+            for chunk in file_obj.chunks():
+                destination.write(chunk)
+            destination.close()
+
+            return Response(f_url, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
